6917c7913568c35679e1b5b6023621550b3e86d9
R. D. Poor <rdpoor@gmail.com>
2022-12-03 15:35:06 -0800

    add more unit tests

--- tests/test_impulse_pe.py
+++ tests/test_impulse_pe.py
@@ -1,6 +1,11 @@
+import os
+import sys
+script_dir = os.path.dirname( __file__ )
+pygmu_dir = os.path.join( script_dir, '..', 'pygmu' )
+sys.path.append( pygmu_dir )
 import unittest
-from pygmu import ImpulsePE
-from pygmu import Extent
+import numpy as np
+from pygmu import (ImpulsePE, Extent, PygPE)
 
 class TestImpulsePE(unittest.TestCase):
 
@@ -8,17 +13,39 @@ class TestImpulsePE(unittest.TestCase):
         pass
 
     def test_init(self):
-        pass
+        self.pe = ImpulsePE(1)
+        self.assertIsInstance(self.pe, ImpulsePE)
 
     def test_render(self):
-        pass
+        e = Extent(-5, 0)
+        self.pe = ImpulsePE(1)
+        expect = np.array([[0], [0], [0], [0], [0]], dtype=np.float32)
+        got = self.pe.render(e)
+        np.testing.assert_array_almost_equal(got, expect)
+
+        e = Extent(0, 5)
+        self.pe = ImpulsePE(1)
+        expect = np.array([[1], [0], [0], [0], [0]], dtype=np.float32)
+        got = self.pe.render(e)
+        np.testing.assert_array_almost_equal(got, expect)
+
+        e = Extent(5, 10)
+        self.pe = ImpulsePE(1)
+        expect = np.array([[0], [0], [0], [0], [0]], dtype=np.float32)
+        got = self.pe.render(e)
+        np.testing.assert_array_almost_equal(got, expect)
 
     def test_extent(self):
-        pass
+        self.pe = ImpulsePE(1)
+        self.assertTrue(Extent().equals(self.pe.extent()))
 
     def test_frame_rate(self):
-        pass
+        self.pe = ImpulsePE(1)
+        self.assertEqual(self.pe.frame_rate(), PygPE.DEFAULT_FRAME_RATE)
 
     def test_channel_count(self):
-        pass
+        self.pe = ImpulsePE(1)
+        self.assertEqual(self.pe.channel_count(), 1)
+        self.pe = ImpulsePE(2)
+        self.assertEqual(self.pe.channel_count(), 2)
 
