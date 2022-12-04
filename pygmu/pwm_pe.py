138f9ed0cda5a8bc4d4791eb8eb8ae01befe77a4
R. D. Poor <rdpoor@gmail.com>
2022-12-03 18:46:00 -0800

    Add comments to pwm_pe

--- pygmu/pwm_pe.py
+++ pygmu/pwm_pe.py
@@ -4,7 +4,17 @@ from pyg_pe import PygPE
 
 class PwmPE(PygPE):
     """
-    Generate a rectangular wave with given period and duty cycle.
+    Generate a rectangular wave with given period and duty cycle.  Note that 
+    period is the number of frames per period, duty_cycle is the ratio of time 
+    it spends = 1.0.  A duty_cycle of 0.0 is low all the time, 0.5 is high half
+    the time, 1.0 is high all the time (like some friends I know).
+
+    Note also that period and duty_cycle can be a constant or the output of 
+    another PE.  
+
+    No attempt is made to protect against oddball values (negative duty cycle,
+    etc).
+
     """
 
     def __init__(self, period:int, duty_cycle:np.float32, channel_count=PygPE.DEFAULT_CHANNEL_COUNT):
@@ -15,17 +25,26 @@ class PwmPE(PygPE):
 
     def render(self, requested:Extent):
         if isinstance(self._period, PygPE):
+            # period is the output of a PE.  Render the values and use them.
             period = self._period.render(requested)
         else:
+            # period is constant.
             period = self._period
 
         if isinstance(self._duty_cycle, PygPE):
+            # duty_cycle is the output of a PE.  Render the values and use them.
             thresh = self._duty_cycle.render(requested) * period
         else:
+            # duty_cycle is constant.  
             thresh = self._duty_cycle * period
 
+        # t is simply a vector of time values.
         t = np.arange(requested.start(), requested.end()).reshape(-1, 1)
+        # t % period counts from 0 to period
+        # (t % period < thresh) is true when t MOD period is less than thresh
+        # (t % period < thresh).astype(np.float32) coerces True=1.0, False=0.0
         v = (t % period < thresh).astype(np.float32)
+        # TODO: handle channel count.  Maybe...
         return v
 
     def channel_count(self):
