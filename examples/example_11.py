9a68eca92a10f8e7e940b23e8f21c329e27abb3f
R. D. Poor <rdpoor@gmail.com>
2022-12-10 11:55:26 -0500

    dynamic f0 and q for BiquadPE

--- examples/example_11.py
+++ examples/example_11.py
@@ -9,12 +9,39 @@ import pygmu as pg
 Test BiquadPE
 """
 
-print("hit return after each example to hear the next")
+src = pg.NoisePE(gain=0.5, channel_count=1).crop(pg.Extent(0,5*44100))
 
-src = pg.NoisePE(gain=1.0, channel_count=1).crop(pg.Extent(0,5*44100))
-
+# constant f0, constant Q
 # gain_db is ignored for lowpass
-pg.Transport(pg.BiquadPE(src, 0.0, 330.0, 40, "lowpass")).play()
+flt = pg.BiquadPE(src, 0.0, 330.0, 40, "lowpass")
+dst = pg.WavWriterPE(flt, "test.wav")
+pg.FtsTransport(dst).play()
+pg.Transport(pg.WavReaderPE("test.wav")).play()
 
 # After the noise source stops, you can hear how long this resonates!
-pg.Transport(pg.BiquadPE(src.gain(0.05), 60.0, 330.0, 10, "peak")).play()
+flt = pg.BiquadPE(src.gain(0.05), 60.0, 330.0, 10, "peak")
+dst = pg.WavWriterPE(flt, "test.wav")
+pg.FtsTransport(dst).play()
+pg.Transport(pg.WavReaderPE("test.wav")).play()
+
+# ramped f0, constant Q
+f0_ramp = pg.LinearRampPE(220, 440, src.extent())
+flt = pg.BiquadPE(src, 0.0, f0_ramp, 40, "lowpass")
+dst = pg.WavWriterPE(flt, "test.wav")
+pg.FtsTransport(dst).play()
+pg.Transport(pg.WavReaderPE("test.wav")).play()
+
+# constant f0, ramped Q
+q_ramp = pg.LinearRampPE(1, 20, src.extent())
+flt = pg.BiquadPE(src, 0.0, 330.0, q_ramp, "lowpass")
+dst = pg.WavWriterPE(flt, "test.wav")
+pg.FtsTransport(dst).play()
+pg.Transport(pg.WavReaderPE("test.wav")).play()
+
+# mix orig with allpass?  Uninteresting, though it might be an error in impl.
+# f0_ramp = pg.LinearRampPE(220, 440, src.extent())
+# flt = pg.BiquadPE(src, 0.0, f0_ramp, 40, "allpass")
+# mix = pg.MixPE(src, flt)
+# dst = pg.WavWriterPE(flt, "test.wav")
+# pg.FtsTransport(dst).play()
+# pg.Transport(pg.WavReaderPE("test.wav")).play()
--- pygmu/biquad_pe.py
+++ pygmu/biquad_pe.py
@@ -21,23 +21,71 @@ class BiquadPE(PygPE):
     LOWSHELF = "lowshelf"
     HIGHSHELF = "highshelf"
 
-    def __init__(self, src_pe, db_gain, center_freq, q, filter_type="bandpass"):
+    def __init__(self, src_pe, db_gain=0, f0=440, q=20, filter_type="lowpass"):
+        """
+        BiQuad filter.  db_gain, f0, q can be constant or other PEs.
+        Note that db_gain is used only for PEAK, NOTCH, LOWSHELF, HIGHSHELF
+        """
         super(BiquadPE, self).__init__()
         self._src_pe = src_pe
         self._db_gain = db_gain
+        self._f0 = f0
+        self._q = q
         self._filter_type = filter_type
         self._a = np.zeros(3, dtype=np.float32)
         self._b = np.zeros(3, dtype=np.float32)
         self._x = np.zeros(3, dtype=np.float32)
         self._y = np.zeros(3, dtype=np.float32)
-        self.set_coefficients(db_gain, center_freq, q)
+        # self.set_coefficients(db_gain, f0, q)
 
     def render(self, requested:Extent):
         src_frames = self._src_pe.render(requested)
+
         # nb: Tricky code: src_frames.shape => (n_frames, n_channels), which 
         # happens to be the two arguments needed by ut.uninitialized_frames()
         dst_frames = ut.uninitialized_frames(*src_frames.shape)
+
+        # optimize case for constant coefficients.
+        all_constant = True
+
+        if isinstance(self._db_gain, PygPE):
+            # db_gain is an array of time-varying numbers
+            db_gain = self._db_gain.render(requested)
+            all_constant = False
+        else:
+            # db_gain is a constant
+            db_gain = self._db_gain
+        if isinstance(self._f0, PygPE):
+            # f0 is an array of time-varying numbers
+            f0 = self._f0.render(requested)
+            all_constant = False
+        else:
+            # f0 is a constant
+            f0 = self._f0
+        if isinstance(self._q, PygPE):
+            # q is an array of time-varying numbers
+            q = self._q.render(requested)
+            all_constant = False
+        else:
+            # q is a constant
+            q = self._q
+
+
+        if all_constant:
+            self.set_coefficients(db_gain, f0, q)
+        else:
+            # this feels really messy.  expand constant coeffs into arrays.
+            if not isinstance(db_gain, np.ndarray):
+                db_gain = ut.const_frames(db_gain, len(src_frames), 1)
+            if not isinstance(f0, np.ndarray):
+                f0 = ut.const_frames(f0, len(src_frames), 1)
+            if not isinstance(q, np.ndarray):
+                q = ut.const_frames(q, len(src_frames), 1)
+
         for i, x in enumerate(src_frames):
+            if all_constant == False:
+                self.set_coefficients(db_gain[i][0], f0[i][0], q[i][0])
+
             y = (self._b[0] * x +
                  self._b[1] * self._x[1] +
                  self._b[2] * self._x[2] -
@@ -59,18 +107,18 @@ class BiquadPE(PygPE):
     def frame_rate(self):
         return self._src_pe.frame_rate()
 
-    def set_coefficients(self, db_gain, center_freq, q):
-        # A is only used for PEAK, NOTCH, SHELF.
-        # TODO: consider different constructors
+    def set_coefficients(self, db_gain, f0, q):
+        # db_gain is only used for PEAK, NOTCH, *SHELF.
+        # TODO: consider different constructors for those...
         A = np.power(10, db_gain / 40)  # ?
-        omega = 2.0 * np.pi * center_freq / self.frame_rate()
+        omega = 2.0 * np.pi * f0 / self.frame_rate()
         sn = np.sin(omega)
         cs = np.cos(omega)
-        print(omega, sn, cs, q)
         alpha = sn / (2.0 * q)
         beta = np.sqrt(A + A)
+        # print(db_gain, f0, q, A, omega, sn, cs, alpha, beta)
 
-        # K_ = np.tan(np.pi * center_freq / self.frame_rate())
+        # K_ = np.tan(np.pi * f0 / self.frame_rate())
         # kSqr_ = K_ * K_
         # denom_ = 1 / (kSqr_ * q + K_ + q)
         # self._a[0] = 1.0
@@ -143,5 +191,5 @@ class BiquadPE(PygPE):
         self._a[1] /= self._a[0]
         self._a[2] /= self._a[0]
 
-        print("a[]=", self._a, "b[]=", self._b)
+        # print("a[]=", self._a, "b[]=", self._b)
         
