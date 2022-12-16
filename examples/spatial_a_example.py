03443b15d612da38657dc12ed3f406e59da14cf7
R. D. Poor <rdpoor@gmail.com>
2022-12-16 14:36:32 -0800

    Improve Spatial args

--- examples/spatial_a_example.py
+++ examples/spatial_a_example.py
@@ -1,73 +1,63 @@
 import os
 import sys
+import numpy as np
+
 script_dir = os.path.dirname( __file__ )
 pygmu_dir = os.path.join( script_dir, '..', 'pygmu' )
 sys.path.append( pygmu_dir )
-import numpy as np
 import pygmu as pg
-import soundfile as sf
 import utils as ut
 
 src = pg.WavReaderPE("samples/s07.wav")
-
-def dtor(degree):
-    """
-    Degrees to Radians
-    """
-    return np.pi * degree / 180.0
-
-def dur(seconds):
-    return int(seconds * src.frame_rate())
 
 t = 0
 def dt(dt):
     global t
     now = t
     t += dt
-    return dur(now)
+    return int(now * src.frame_rate())
 
 mix = pg.MixPE(
-    # immobile (for reference)
-    pg.SpatialAPE(src, theta=dtor(0)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(0)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(0)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(0)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(0)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(0)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(0)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(0)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(0)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(0)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(0)).delay(dt(1.5)),
-
-    # pan using delay only
-    pg.SpatialAPE(src, theta=dtor(90)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(72)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(54)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(36)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(18)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(0)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(-18)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(-36)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(-54)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(-72)).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(-90)).delay(dt(1.5)),
+    # pan using delay and gain
+    # Note: cosine curve attempts to overcome "hole in the middle"
+    pg.SpatialAPE(src, degree=90, curve='cosine').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=72, curve='cosine').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=54, curve='cosine').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=36, curve='cosine').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=18, curve='cosine').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=0, curve='cosine').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=-18, curve='cosine').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=-36, curve='cosine').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=-54, curve='cosine').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=-72, curve='cosine').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=-90, curve='cosine').delay(dt(1.5)),
 
     # pan using delay and gain
-    # Note: gain_[lr] should not be linear since it leaves "hole in the middle"
-    # with gain_l=0.5 and gain_r=0.5 (but still convincing sounding).  More 
-    # likely that should be 0.7 or so.  Perhaps use db attenuation instead?
-    pg.SpatialAPE(src, theta=dtor(90), gain_l=0.0, gain_r=1.0).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(72), gain_l=0.1, gain_r=0.9).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(54), gain_l=0.2, gain_r=0.8).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(36), gain_l=0.3, gain_r=0.7).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(18), gain_l=0.4, gain_r=0.6).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(0), gain_l=0.5, gain_r=0.5).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(-18), gain_l=0.6, gain_r=0.4).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(-36), gain_l=0.7, gain_r=0.3).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(-54), gain_l=0.8, gain_r=0.2).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(-72), gain_l=0.9, gain_r=0.1).delay(dt(0.25)),
-    pg.SpatialAPE(src, theta=dtor(-90), gain_l=1.0, gain_r=0.0).delay(dt(0.25)),
+    # Note: linear leaves a "hole in the middle"
+    pg.SpatialAPE(src, degree=90, curve='linear').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=72, curve='linear').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=54, curve='linear').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=36, curve='linear').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=18, curve='linear').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=0, curve='linear').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=-18, curve='linear').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=-36, curve='linear').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=-54, curve='linear').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=-72, curve='linear').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=-90, curve='linear').delay(dt(1.5)),
+
+    # pan using delay only
+    pg.SpatialAPE(src, degree=90, curve='none').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=72, curve='none').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=54, curve='none').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=36, curve='none').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=18, curve='none').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=0, curve='none').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=-18, curve='none').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=-36, curve='none').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=-54, curve='none').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=-72, curve='none').delay(dt(0.25)),
+    pg.SpatialAPE(src, degree=-90, curve='none').delay(dt(1.5)),
 
 )
 
--- pygmu/spatial_a_pe.py
+++ pygmu/spatial_a_pe.py
@@ -2,6 +2,13 @@ import numpy as np
 from extent import Extent
 from pyg_pe import PygPE
 from mono_pe import MonoPE
+import utils as ut
+
+"""
+Notes:
+https://www.martinjaroszewicz.com/book/spatial_audio.html
+https://jeffvautin.com/2015/03/computing-panning-curves/
+"""
 
 """
 Stereo to mono frames and back again:
@@ -37,12 +44,28 @@ class SpatialAPE(PygPE):
     HEAD_RADIUS_M = 0.22
 
 
-    def __init__(self, src_pe, theta:float=0, gain_l:float=1.0, gain_r:float=1.0):
+    def __init__(self, src_pe, degree=0, theta=None, curve='cosine'):
+        """
+        Create a panner.  
+        src_pe: the source frames
+        degree: -90 = hard left, 0 = center, 90 = hard right
+        theta: if given, overrides degree: -pi/2 = hard left, 0=center, pi/2=hard right
+        curve: one of the following:  As theta goes from hard left to hard right:
+          'none' - no amplitude scaling -- time delay only
+          'linear' - left amplitude ramps linearly 1.0 to 0.0, right from 0.0 to 1.0, 0.5 at center
+          'cosine' - left ramps by cosine from 1.0 to 0.0, right ramps by sin, 0.7 at center
+        """
         super(SpatialAPE, self).__init__()
         self._src_pe = src_pe
-        self.setup_chains(theta, gain_l, gain_r)
+        if theta is None:
+            theta = np.pi * degree / 180.0
+        self.setup_chains(theta, curve)
 
     def render(self, requested:Extent):
+        overlap = self._src_pe.extent().intersect(requested)
+        if overlap.is_empty():
+            return ut.const_frames(0.0, requested.duration(), 2)
+
         left_frames = self._left_chain.render(requested)
         right_frames = self._right_chain.render(requested)
         return np.hstack((left_frames, right_frames))
@@ -65,7 +88,7 @@ class SpatialAPE(PygPE):
         dly_frames = dly_s * self.frame_rate()
         return dly_frames
 
-    def setup_chains(self, theta, gain_l, gain_r):
+    def setup_chains(self, theta, curve):
         self._left_chain = MonoPE(self._src_pe)
         self._right_chain = MonoPE(self._src_pe)
 
@@ -76,8 +99,17 @@ class SpatialAPE(PygPE):
         elif delay < 0:
             self._right_chain = self._right_chain.delay(int(-delay))
 
-        if gain_l != 1.0:
-            self._left_chain = self._left_chain.gain(gain_l)
+        if curve == 'linear':
+            # norm_angle goes from 0.0 to 1.0 as theta goes from hard left to hard right 
+            norm_angle = ut.lerp(theta, -np.pi/2, np.pi/2, 0, 1)
+            self._left_chain = self._left_chain.gain(1-norm_angle)
+            self._right_chain = self._right_chain.gain(norm_angle)
+        elif curve == 'cosine':
+            # omega goes from 0 to pi/2 as theta goes from hard left to hard right
+            omega = (theta + np.pi/2)/2
+            self._left_chain = self._left_chain.gain(np.cos(omega))
+            self._right_chain = self._right_chain.gain(np.sin(omega))
+        else:
+            # gain is unchanged
+            pass
 
-        if gain_r != 1.0:
-            self._right_chain = self._right_chain.gain(gain_r)
