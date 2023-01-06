24512bcf7b35b108a8e11a12a40ca6465833c213
R. D. Poor <rdpoor@gmail.com>
2023-01-06 07:56:55 -0800

    update test nomenclature

--- tests/test_sequence_pe.py
+++ tests/test_sequence_pe.py
@@ -11,12 +11,12 @@ class TestSequencePE(unittest.TestCase):
 
     def setUp(self):
         self.tvs = np.array([[0, 1.0], [4, 2.0], [6, -3.0]], dtype=np.float32)
-        self.pe_a = SequencePE(self.tvs, interpolation=SequencePE.RAMP)
-        self.pe_b = SequencePE(self.tvs, interpolation=SequencePE.STEP)
+        self.pe_ramp = SequencePE(self.tvs, interpolation=SequencePE.RAMP)
+        self.pe_step = SequencePE(self.tvs, interpolation=SequencePE.STEP)
 
     def test_init(self):
-        self.assertIsInstance(self.pe_a, SequencePE)
-        self.assertIsInstance(self.pe_b, SequencePE)
+        self.assertIsInstance(self.pe_ramp, SequencePE)
+        self.assertIsInstance(self.pe_step, SequencePE)
 
     def test_insertion(self):
     	pe = SequencePE([[0, 100], [1, 200]])
@@ -33,46 +33,46 @@ class TestSequencePE(unittest.TestCase):
     	np.testing.assert_array_almost_equal(pe._times, [0, 1])
     	np.testing.assert_array_almost_equal(pe._values, [100, 300])
 
-    	np.testing.assert_array_almost_equal(self.pe_a._times, [0, 4, 6])
-    	np.testing.assert_array_almost_equal(self.pe_a._values, [1.0, 2.0, -3.0])
+    	np.testing.assert_array_almost_equal(self.pe_ramp._times, [0, 4, 6])
+    	np.testing.assert_array_almost_equal(self.pe_ramp._values, [1.0, 2.0, -3.0])
 
     def test_render(self):
         e = Extent(-5, 0)     # no overlap -- return first value
         expect = np.full([5, 1], 1.0, dtype=np.float32)
-        got = self.pe_a.render(e)
+        got = self.pe_ramp.render(e)
         np.testing.assert_array_almost_equal(got, expect)
-        got = self.pe_b.render(e)
+        got = self.pe_step.render(e)
         np.testing.assert_array_almost_equal(got, expect)
 
         e = Extent(6, 11)     # no overlap -- return last value
         expect = np.full([5, 1], -3.0, dtype=np.float32)
-        got = self.pe_a.render(e)
+        got = self.pe_ramp.render(e)
         np.testing.assert_array_almost_equal(got, expect)
-        got = self.pe_b.render(e)
+        got = self.pe_step.render(e)
         np.testing.assert_array_almost_equal(got, expect)
 
         e = Extent(-1, 8)
         # interpolation = ramp
         #           idx =  -1   0    1     2    3     4    5      6     7
         expect = np.array([1.0, 1.0, 1.25, 1.5, 1.75, 2.0, -0.5, -3.0, -3.0]).reshape(-1, 1)
-        got = self.pe_a.render(e)
+        got = self.pe_ramp.render(e)
         np.testing.assert_array_almost_equal(got, expect)
 
         # interpolation = step
         #           idx =  -1   0    1    2    3    4    5     6     7
         expect = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 2.0, 2.0, -3.0, -3.0]).reshape(-1, 1)
-        got = self.pe_b.render(e)
+        got = self.pe_step.render(e)
         np.testing.assert_array_almost_equal(got, expect)
 
     def test_extent(self):
-        self.assertTrue(Extent().equals(self.pe_a.extent()))
-        self.assertTrue(Extent().equals(self.pe_b.extent()))
+        self.assertTrue(Extent().equals(self.pe_ramp.extent()))
+        self.assertTrue(Extent().equals(self.pe_step.extent()))
 
     def test_frame_rate(self):
-        self.assertEqual(self.pe_a.frame_rate(), PygPE.DEFAULT_FRAME_RATE)
-        self.assertEqual(self.pe_b.frame_rate(), PygPE.DEFAULT_FRAME_RATE)
+        self.assertEqual(self.pe_ramp.frame_rate(), PygPE.DEFAULT_FRAME_RATE)
+        self.assertEqual(self.pe_step.frame_rate(), PygPE.DEFAULT_FRAME_RATE)
 
     def test_channel_count(self):
-        self.assertEqual(self.pe_a.channel_count(), 1)
-        self.assertEqual(self.pe_b.channel_count(), 1)
+        self.assertEqual(self.pe_ramp.channel_count(), 1)
+        self.assertEqual(self.pe_step.channel_count(), 1)
 
