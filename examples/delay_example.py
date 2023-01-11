86ae24825717d5985ca30f2149ebb98343425216
R. D. Poor <rdpoor@gmail.com>
2023-01-11 05:49:31 -0800

    merge delay_a_pe => delay_pe

---
+++ examples/delay_example.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import numpy as np
+script_dir = os.path.dirname( __file__ )
+pygmu_dir = os.path.join( script_dir, '..', 'pygmu' )
+sys.path.append( pygmu_dir )
+import pygmu as pg
+"""
+Test TimewarpPE
+"""
+
+FRAME_RATE = 48000
+
+# mixing original with time-varying delay == flanging
+source = pg.WavReaderPE("samples/Tamper_MagnifyingFrame1.wav")
+warped = pg.DelayPE(source, pg.SinPE(frequency=1, amplitude=100.0, frame_rate=FRAME_RATE))
+# DelayPE with a time-varying delay has infinite extent.  Crop it to the length of the original
+warped = warped.crop(source.extent())
+
+pg.Transport(pg.MixPE(source, warped)).play()
