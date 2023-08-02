f6a76c9c6048efd0245023e209fd1a871a39e49a
R. D. Poor <rdpoor@gmail.com>
2023-08-02 13:55:14 -0700

    update tralfram_test

--- tests/tralfam_test.py
+++ tests/tralfam_test.py
@@ -1,5 +1,19 @@
-import pygmu as pg
+import os
+import sys
+script_dir = os.path.dirname( __file__ )
+pygmu_dir = os.path.join( script_dir, '..', 'pygmu' )
+sys.path.append( pygmu_dir )
+import unittest
 import numpy as np
+import pyg_exceptions as pyx
+import pygmu as pg
+from pygmu import (PygPE, WavReaderPE)
 
-pluck = pg.WavReaderPE('../samples/044-n-2723_stero.wav')
-pg.Transport(pg.TralfamPE(pluck)).play()
+pluck = pg.WavReaderPE('./samples/music/044-n-2723_stero.wav')
+stretched = pluck.tralfam()
+dur = stretched.extent().duration()
+looped = stretched.loop(dur).crop(pg.Extent(0, 10*dur))
+looped2 = stretched.loop(dur).time_shift(dur/2).crop(pg.Extent(0, 10*dur))
+mix = pg.MixPE(looped, looped2)
+# pg.Transport(pg.TralfamPE(looped)).play()
+mix.pygplay('nooootttteeee')
